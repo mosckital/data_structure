25d5526e23bf22abf10da29a13c540c881925cb5
Kaiyan <k.max.xiao@gmail.com>
2020-05-01 23:56:17 +0100

    Added type hints and docstring for Array and its unit tests. (#B1)

--- data_structures/array.py
+++ data_structures/array.py
@@ -1,54 +1,144 @@
-class Array(object):
+"""Custom implementation of a fixed length array.
+
+This module illustrates the foundation knowledge of a fixed length array by
+implementing it. It has no practical usage but only serves as an data structure
+exercise.
+"""
+from typing import TypeVar, Generic, Optional, List
+
+
+KT = TypeVar('KT')
+"""type: The generic type to use in the Array definition."""
+
+class Array(Generic[KT]):
     """
-    This is a wrapper of list to illustrate the operations of an array of fixed
-    length for the purpose of learning data structures.
+    Array[T](n) -> a fixed length array of size `n` for values of type `T`
+
+    All slots are initialised to `None`.
+
+    This is a custom implementation of a Fixed Length Array for learning
+    purpose. The implementation is based on a pre-allocated list of known length
+    and the length will keep unchanged.
+
+    Args:
+        max_size: the maximum size of the array
+
+    Attributes:
+        data (List[Optional[KT]]): a list of length max_size to store data
+        max_size (int): the max size of the array
+        curr_size (int): the current size of the array
     """
 
     def __init__(self, max_size: int):
         self.data = [None] * max_size
         self.max_size = max_size
         self.curr_size = 0
-    
+
     def get_size(self) -> int:
+        """
+        Get the current size of the array.
+
+        Returns:
+            The current size of the array
+        """
         return self.curr_size
-    
-    def index_of(self, e) -> int:
+
+    def index_of(self, val: KT) -> int:
+        """
+        Get the index of a value, or -1 if not found.
+
+        Args:
+            val: the value to look for
+
+        Returns:
+            The index of the value or -1 if not found
+        """
         for i in range(self.curr_size):
-            if self.data[i] == e:
+            if self.data[i] == val:
                 return i
         return -1
 
-    def value_at(self, idx: int):
+    def value_at(self, idx: int) -> Optional[KT]:
+        """
+        Get the value at the given index.
+
+        Args:
+            idx: the index to fetch
+
+        Returns:
+            The value at the given index or None if index not valid
+        """
         if 0 <= idx < self.curr_size:
             return self.data[idx]
-        else:
-            return None
-    
-    def insert_at(self, idx: int, e) -> bool:
+        return None
+
+    def insert_at(self, idx: int, val: KT) -> bool:
+        """
+        Insert a value at the given index.
+
+        Note:
+            The value will not be inserted if the index is not valid or the
+            array is already full.
+
+        Args:
+            idx: the index to insert at
+            val: the value to insert
+
+        Returns:
+            True if insertion is successful or False otherwise
+        """
         if 0 <= idx <= self.curr_size < self.max_size:
             for i in range(self.curr_size, idx, -1):
                 self.data[i] = self.data[i - 1]
-            self.data[idx] = e
+            self.data[idx] = val
             self.curr_size += 1
             return True
-        else:
-            return False
+        return False
 
     def delete_at(self, idx: int) -> bool:
+        """
+        Delete an element at the given index.
+
+        Note:
+            The deletion will not perform if the index is not valid.
+
+        Args:
+            idx: the index to perform deletion
+
+        Returns:
+            True if deletion is successful or False otherwise
+        """
         if 0 <= idx < self.curr_size:
             for i in range(idx, self.curr_size - 1):
                 self.data[i] = self.data[i + 1]
             self.curr_size -= 1
             return True
-        else:
-            return False
-    
-    def update_at(self, idx: int, e) -> bool:
+        return False
+
+    def update_at(self, idx: int, val: KT) -> bool:
+        """
+        Update an element at the given index by the given value.
+
+        Note:
+            The update will not perform if the index is not valid.
+
+        Args:
+            idx: the index to update
+            val: the new value
+
+        Returns:
+            True if update is successful or False otherwise
+        """
         if 0 <= idx < self.curr_size:
-            self.data[idx] = e
+            self.data[idx] = val
             return True
-        else:
-            return False
-    
-    def traverse(self) -> list:
+        return False
+
+    def traverse(self) -> List[KT]:
+        """
+        Traverse all elements in the array and return them in a list.
+
+        Returns:
+            A list containing all elements of the array in order
+        """
         return [self.data[i] for i in range(self.curr_size)]
--- tests_data_structures/test_array.py
+++ tests_data_structures/test_array.py
@@ -1,10 +1,19 @@
-import pytest
+"""Test suite for the Array class which implements a fixed length array.
+
+The main idea of the tests is to compare the execution results of the class to
+test with the results of a reference class. The results should always match.
+"""
 from random import choice, randint
 from enum import Enum
+from typing import TypeVar, Generic, Optional, List, Any
+import pytest
 from data_structures.array import Array
 
 
 class Op(Enum):
+    """
+    An enum class to list all eligible operations on an Array.
+    """
     GET_SIZE = 0
     INDEX_OF = 1
     VALUE_AT = 2
@@ -14,61 +23,213 @@ class Op(Enum):
     TRAVERSE = 6
 
 
-class RefArray(object):
+KR = TypeVar('KR')
+
+
+class RefArray(Generic[KR]):
+    """
+    The reference implementation of fixed length array, to be used to test the
+    correctness of the custom implementation.
+
+    Attributes:
+        data (List[KR]): a list to store data
+        max_size (int): the max size of the array
+    """
 
     def __init__(self, max_size: int):
         self.data = []
         self.max_size = max_size
-    
+
     def get_size(self) -> int:
+        """
+        Get the current size of the array.
+
+        Returns:
+            The current size of the array
+        """
         return len(self.data)
-    
-    def index_of(self, e) -> int:
+
+    def index_of(self, val: KR) -> int:
+        """
+        Get the index of a value, or -1 if not found.
+
+        Args:
+            val: the value to look for
+
+        Returns:
+            The index of the value or -1 if not found
+        """
         try:
-            return self.data.index(e)
-        except ValueError as e:
+            return self.data.index(val)
+        except ValueError:
             return -1
-    
-    def value_at(self, idx: int):
+
+    def value_at(self, idx: int) -> Optional[KR]:
+        """
+        Get the value at the given index.
+
+        Args:
+            idx: the index to fetch
+
+        Returns:
+            The value at the given index or None if index not valid
+        """
         if 0 <= idx < len(self.data):
             return self.data[idx]
-        else:
-            return None
-    
-    def insert_at(self, idx: int, e) -> bool:
+        return None
+
+    def insert_at(self, idx: int, val: KR) -> bool:
+        """
+        Insert a value at the given index.
+
+        Note:
+            The value will not be inserted if the index is not valid or the
+            array is already full.
+
+        Args:
+            idx: the index to insert at
+            val: the value to insert
+
+        Returns:
+            True if insertion is successful or False otherwise
+        """
         if len(self.data) == self.max_size:
             return False
         if 0 <= idx <= len(self.data):
-            self.data.insert(idx, e)
+            self.data.insert(idx, val)
             return True
-        else:
-            return False
-    
+        return False
+
     def delete_at(self, idx: int) -> bool:
+        """
+        Delete an element at the given index.
+
+        Note:
+            The deletion will not perform if the index is not valid.
+
+        Args:
+            idx: the index to perform deletion
+
+        Returns:
+            True if deletion is successful or False otherwise
+        """
         if 0 <= idx < len(self.data):
             self.data[idx:idx + 1] = []
             return True
-        else:
-            return False
-    
-    def update_at(self, idx: int, e) -> bool:
+        return False
+
+    def update_at(self, idx: int, val: KR) -> bool:
+        """
+        Update an element at the given index by the given value.
+
+        Note:
+            The update will not perform if the index is not valid.
+
+        Args:
+            idx: the index to update
+            val: the new value
+
+        Returns:
+            True if update is successful or False otherwise
+        """
         if 0 <= idx < len(self.data):
-            self.data[idx] = e
+            self.data[idx] = val
             return True
-        else:
-            return False
-        
-    def traverse(self) -> list:
+        return False
+
+    def traverse(self) -> List[KR]:
+        """
+        Traverse all elements in the array and return them in a list.
+
+        Returns:
+            A list containing all elements of the array in order
+        """
         return self.data
-    
-    def __eq__(self, value):
+
+    def __eq__(self, value: Any) -> bool:
+        """
+        Overwritten to permit the comparison in format [RefArray == Array]. It
+        is not valid for the inverse order.
+        """
         if isinstance(value, Array):
             return len(self.data) == value.curr_size and \
                 self.data == value.data[:value.curr_size]
         return super().__eq__(value)
 
 
-class TestArray(object):  
+class TestArray():
+    """
+    The test suite class for the Array class.
+    """
+
+    @staticmethod
+    def _check_op_get_size(arr, alt):
+        assert arr.get_size() == alt.get_size()
+
+    @staticmethod
+    def _check_op_index_of(arr, alt, i):
+        # check normal case
+        if arr.get_size() > 0:
+            idx = randint(0, arr.get_size() - 1)
+            tmp = arr.value_at(idx)
+            assert arr.index_of(tmp) == alt.index_of(tmp) == idx
+        # check two edge cases
+        tmp = randint(i, i * 2)
+        assert arr.index_of(tmp) == alt.index_of(tmp) == -1
+        assert arr.index_of(-tmp) == alt.index_of(-tmp) == -1
+
+    @staticmethod
+    def _check_op_value_at(arr, alt):
+        # check normal case
+        if arr.get_size() > 0:
+            idx = randint(0, arr.get_size() - 1)
+            assert arr.value_at(idx) == alt.value_at(idx)
+            assert arr.value_at(idx) is not None
+        # check two edge cases
+        idx = randint(arr.get_size(), arr.get_size() * 2)
+        assert arr.value_at(idx) == alt.value_at(idx) == None
+        idx = randint(- arr.get_size() - 1, -1)
+        assert arr.value_at(idx) == alt.value_at(idx) == None
+
+    @staticmethod
+    def _check_op_insert_at(arr, alt, i, max_size):
+        # check two edge cases
+        idx = randint(arr.get_size() + 1, arr.get_size() * 2 + 1)
+        assert arr.insert_at(idx, i) == alt.insert_at(idx, i) == False
+        idx = randint(- arr.get_size() - 1, -1)
+        assert arr.insert_at(idx, i) == alt.insert_at(idx, i) == False
+        # check normal case
+        idx = randint(0, arr.get_size())
+        ret = bool(arr.get_size() < max_size)
+        assert arr.insert_at(idx, i) == alt.insert_at(idx, i) == ret
+
+    @staticmethod
+    def _check_op_delete_at(arr, alt):
+        # check two edge cases
+        idx = randint(arr.get_size(), arr.get_size() * 2)
+        assert arr.delete_at(idx) == alt.delete_at(idx) == False
+        idx = randint(- arr.get_size() - 1, -1)
+        assert arr.delete_at(idx) == alt.delete_at(idx) == False
+        # check normal case
+        if arr.get_size() > 0:
+            idx = randint(0, arr.get_size() - 1)
+            assert arr.delete_at(idx) == alt.delete_at(idx) == True
+
+    @staticmethod
+    def _check_op_update_at(arr, alt, i):
+        # check two edge cases
+        idx = randint(arr.get_size(), arr.get_size() * 2)
+        assert arr.update_at(idx, i) == alt.update_at(idx, i) == False
+        idx = randint(- arr.get_size() - 1, -1)
+        assert arr.update_at(idx, i) == alt.update_at(idx, i) == False
+        # check normal case
+        if arr.get_size() > 0:
+            idx = randint(0, arr.get_size() - 1)
+            assert arr.update_at(idx, i) == alt.update_at(idx, i) == True
+
+    @staticmethod
+    def _check_op_traverse(arr, alt):
+        assert arr.traverse() == alt.traverse()
 
     @pytest.mark.parametrize(
         'max_size',
@@ -78,71 +239,33 @@ class TestArray(object):
         'n_ops',
         [100, 1000, 10000],
     )
-    def test_random_ops(self, max_size, n_ops):
+    def test_random_ops(self, max_size: int, n_ops: int):
+        """
+        Randomly test the operations of the Array class.
+        """
         # the array implementation to test
-        arr = Array(max_size)
+        arr = Array[int](max_size)
         # the reference array implementation
         alt = RefArray(max_size)
         # list of operations to test
-        ops = [o for o in Op]
+        ops = list(Op)
         # perform n_ops times of operation test
         for i in range(n_ops):
             # randomly choose an operation
-            op = choice(ops)
-            if op == Op.GET_SIZE:
-                assert arr.get_size() == alt.get_size()
-            elif op == Op.INDEX_OF:
-                # check normal case
-                if arr.get_size() > 0:
-                    idx = randint(0, arr.get_size() - 1)
-                    e = arr.value_at(idx)
-                    assert arr.index_of(e) == alt.index_of(e) == idx
-                # check two edge cases
-                e = randint(i, i * 2)
-                assert arr.index_of(e) == alt.index_of(e) == -1
-                assert arr.index_of(-e) == alt.index_of(-e) == -1
-            elif op == Op.VALUE_AT:
-                # check normal case
-                if arr.get_size() > 0:
-                    idx = randint(0, arr.get_size() - 1)
-                    assert arr.value_at(idx) == alt.value_at(idx)
-                    assert arr.value_at(idx) is not None
-                # check two edge cases
-                idx = randint(arr.get_size(), arr.get_size() * 2)
-                assert arr.value_at(idx) == alt.value_at(idx) == None
-                idx = randint(- arr.get_size() - 1, -1)
-                assert arr.value_at(idx) == alt.value_at(idx) == None
-            elif op == Op.INSERT_AT:
-                # check two edge cases
-                idx = randint(arr.get_size() + 1, arr.get_size() * 2 + 1)
-                assert arr.insert_at(idx, i) == alt.insert_at(idx, i) == False
-                idx = randint(- arr.get_size() - 1, -1)
-                assert arr.insert_at(idx, i) == alt.insert_at(idx, i) == False
-                # check normal case
-                idx = randint(0, arr.get_size())
-                ret = True if arr.get_size() < max_size else False
-                assert arr.insert_at(idx, i) == alt.insert_at(idx, i) == ret
-            elif op == Op.DELETE_AT:
-                # check two edge cases
-                idx = randint(arr.get_size(), arr.get_size() * 2)
-                assert arr.delete_at(idx) == alt.delete_at(idx) == False
-                idx = randint(- arr.get_size() - 1, -1)
-                assert arr.delete_at(idx) == alt.delete_at(idx) == False
-                # check normal case
-                if arr.get_size() > 0:
-                    idx = randint(0, arr.get_size() - 1)
-                    assert arr.delete_at(idx) == alt.delete_at(idx) == True
-            elif op == Op.UPDATE_AT:
-                # check two edge cases
-                idx = randint(arr.get_size(), arr.get_size() * 2)
-                assert arr.update_at(idx, i) == alt.update_at(idx, i) == False
-                idx = randint(- arr.get_size() - 1, -1)
-                assert arr.update_at(idx, i) == alt.update_at(idx, i) == False
-                # check normal case
-                if arr.get_size() > 0:
-                    idx = randint(0, arr.get_size() - 1)
-                    assert arr.update_at(idx, i) == alt.update_at(idx, i) == True
-            elif op == Op.TRAVERSE:
-                assert arr.traverse() == alt.traverse()
+            op_to_check = choice(ops)
+            if op_to_check == Op.GET_SIZE:
+                self._check_op_get_size(arr, alt)
+            elif op_to_check == Op.INDEX_OF:
+                self._check_op_index_of(arr, alt, i)
+            elif op_to_check == Op.VALUE_AT:
+                self._check_op_value_at(arr, alt)
+            elif op_to_check == Op.INSERT_AT:
+                self._check_op_insert_at(arr, alt, i, max_size)
+            elif op_to_check == Op.DELETE_AT:
+                self._check_op_delete_at(arr, alt)
+            elif op_to_check == Op.UPDATE_AT:
+                self._check_op_update_at(arr, alt, i)
+            elif op_to_check == Op.TRAVERSE:
+                self._check_op_traverse(arr, alt)
             # check two array implementations are same after any operation
             assert alt == arr
